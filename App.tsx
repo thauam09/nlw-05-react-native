2267083511cf279a8f208ef74835d19272bb03e5
thauam09 <thauam_09martins@hotmail.com>
2021-04-21 01:45:28 -0300

    Aula 2 - Criado projeto e página de boas vindas

--- App.tsx
+++ App.tsx
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Welcome } from "./src/pages/Welcome";
+// import GlobalStyles from "./src/GlobalStyles";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Welcome />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
